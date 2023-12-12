e28c23e0cc82021fd08df9f7497c42bf409456c9
Rick South <rick@natamus.com>
2023-12-12 10:02:44 +0100

    GUI Clock source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/guiclock/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.guiclock.util;
+
+public class Reference {
+	public static final String MOD_ID = "guiclock";
+	public static final String NAME = "GUI Clock";
+	public static final String VERSION = "4.2";
+	public static final String ACCEPTED_VERSIONS = "[1.19.4]";
+}
