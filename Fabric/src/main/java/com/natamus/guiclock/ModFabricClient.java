722674b1dc9cf5e75192d3c724584b28c26f87a7
Rick South <rick@natamus.com>
2023-12-12 10:02:55 +0100

    GUI Clock source code for 1.20.1.

---
+++ Fabric/src/main/java/com/natamus/guiclock/ModFabricClient.java
@@ -0,0 +1,19 @@
+package com.natamus.guiclock;
+
+import com.natamus.guiclock.events.GUIEvent;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.gui.GuiGraphics;
+
+public class ModFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() { 
+		registerEvents();
+	}
+	
+	private void registerEvents() {
+		HudRenderCallback.EVENT.register((GuiGraphics guiGraphics, float tickDelta) -> {
+			GUIEvent.renderOverlay(guiGraphics, tickDelta);
+		});
+	}
+}
---
+++ Forge/src/main/java/com/natamus/guiclock/forge/events/ForgeGUIEvent.java
@@ -0,0 +1,20 @@
+package com.natamus.guiclock.forge.events;
+
+import com.natamus.guiclock.events.GUIEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeGUIEvent extends Gui {
+	public ForgeGUIEvent(Minecraft mc, ItemRenderer itemRenderer){
+		super(mc, itemRenderer);
+	}
+	
+	@SubscribeEvent(priority = EventPriority.NORMAL)
+	public void renderOverlay(RenderGuiOverlayEvent.Post e) {
+		GUIEvent.renderOverlay(e.getGuiGraphics(), e.getPartialTick());
+	}
+}
