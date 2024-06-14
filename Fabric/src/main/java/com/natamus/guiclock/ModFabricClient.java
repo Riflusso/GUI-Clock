43e87383aac0dc715d2e6c36f103fd9c57acfde1
Rick South <rick@natamus.com>
2024-06-14 05:30:14 +0200

    GUI Clock source code for 1.21.0.

---
+++ Fabric/src/main/java/com/natamus/guiclock/ModFabricClient.java
@@ -0,0 +1,14 @@
+package com.natamus.guiclock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() { 
+		registerEvents();
+	}
+	
+	private void registerEvents() {
+
+	}
+}
